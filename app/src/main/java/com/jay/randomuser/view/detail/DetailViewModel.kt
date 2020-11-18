9cea794fa1fea161ccb8db2b38d2a71716f034f7
wj1227 <wj122791@gmail.com>
2020-11-18 16:19:52 +0900

    [완성]

--- app/src/main/java/com/jay/randomuser/view/detail/DetailViewModel.kt
+++ app/src/main/java/com/jay/randomuser/view/detail/DetailViewModel.kt
@@ -3,21 +3,29 @@ package com.jay.randomuser.view.detail
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.jay.randomuser.data.response.UserResponse
+import com.jay.randomuser.utils.lifecycle.SingleData
 import com.jay.randomuser.view.base.BaseViewModel
 import com.jay.randomuser.view.base.ViewModelType
 import com.jay.randomuser.view.detail.mapper.UserDetailMapper
 import com.jay.randomuser.view.detail.model.UserDetailModel
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.BiFunction
 import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.PublishSubject
 
 interface DetailViewModelType: ViewModelType<DetailViewModelType.Input, DetailViewModelType.Output> {
     interface Input {
-
+        fun cellPhoneClick()
+        fun phoneClick()
+        fun emailClick()
     }
 
     interface Output {
         val user: LiveData<UserDetailModel>
+        val email: LiveData<SingleData<String>>
+        val cellPhone: LiveData<SingleData<String>>
+        val phone: LiveData<SingleData<String>>
     }
 }
 
@@ -36,6 +44,21 @@ class DetailViewModel(
     override val user: LiveData<UserDetailModel>
         get() = _user
 
+    private val _emailSubject: PublishSubject<Unit> = PublishSubject.create()
+    private val _email: MutableLiveData<SingleData<String>> = MutableLiveData()
+    override val email: LiveData<SingleData<String>>
+        get() = _email
+
+    private val _cellPhoneSubject: PublishSubject<Unit> = PublishSubject.create()
+    private val _cellPhone: MutableLiveData<SingleData<String>> = MutableLiveData()
+    override val cellPhone: LiveData<SingleData<String>>
+        get() = _cellPhone
+
+    private val _phoneSubject: PublishSubject<Unit> = PublishSubject.create()
+    private val _phone: MutableLiveData<SingleData<String>> = MutableLiveData()
+    override val phone: LiveData<SingleData<String>>
+        get() = _phone
+
     init {
         val user = Observable.just(user)
             .replay(1)
@@ -48,5 +71,38 @@ class DetailViewModel(
         _userSubject.observeOn(AndroidSchedulers.mainThread())
             .subscribe(_user::setValue)
             .let(compositeDisposable::add)
+
+        _cellPhoneSubject.withLatestFrom(user, BiFunction { _: Unit, user: UserResponse -> user })
+            .map { it.cell.trim() }
+            .map { SingleData(it) }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(_cellPhone::setValue)
+            .let(compositeDisposable::add)
+
+        _phoneSubject.withLatestFrom(user, BiFunction { _: Unit, user: UserResponse -> user })
+            .map { it.phone.trim() }
+            .map { SingleData(it) }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(_phone::setValue)
+            .let(compositeDisposable::add)
+
+        _emailSubject.withLatestFrom(user, BiFunction { _: Unit, user: UserResponse -> user })
+            .map { it.email}
+            .map { SingleData(it) }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(_email::setValue)
+            .let(compositeDisposable::add)
+    }
+
+    override fun cellPhoneClick() {
+        _cellPhoneSubject.onNext(Unit)
+    }
+
+    override fun phoneClick() {
+        _phoneSubject.onNext(Unit)
+    }
+
+    override fun emailClick() {
+        _emailSubject.onNext(Unit)
     }
 }
